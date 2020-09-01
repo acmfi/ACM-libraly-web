d187d4cbd067a461250013fa065a316307346841
FORGIS98 <cifradoforgis@gmail.com>
2020-09-02 00:11:53 +0200

    Pilla los errores 404 y demas correctamente :D
    Antes todo era 200 pasase lo que pasase...ahora no :) ahora ya pilla el
    404 :D

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const fetch = require('node-fetch')
 
 const app = express();
@@ -14,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('The best web ever is running on port 3000!');
 });
 
 app.post('/', function (req, res) {
@@ -24,24 +23,25 @@ app.post('/', function (req, res) {
   let isISBN = book.match(/^(97(8|9))?\d{9}(\d|X)$/)
 
   fetch(url, {
-    method: "get"
+    methos: "get"
   })
-    .then((res) => res.json())
-    .then((data) => {
-      // TODO: Cambiar BOOM para que devuelva un 404 cuando no se encuentre el libro!
-      // Y así poder hacer "res.responseCode != 200" (responseCode o algo asi es :/)
-      if(data.length == 0)
-        res.render('index', {book: null, error: 'Error: Libro no encontrado!'})
-      else{
-        if(isISBN){
-          let bookInfo = `El libro \"${data.title}\" ha sido escrito por \"${data.author}\".`
-          res.render('index', {book: bookInfo, error: null})
-        }else{
-          let bookInfo = `El libro \"${data[0].title}\" ha sido escrito por \"${data[0].author}\".`
-          res.render('index', {book: bookInfo, error: null})
-        }
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        res.render('index', {book: null, error: 'Error: Libro no encontrado!'});
       }
     })
-    .catch((err) => console.log(err));
+    .then(data => {
+      console.log(data)
+      if(isISBN){
+        let bookInfo = `El libro \"${data.title}\" ha sido escrito por \"${data.author}\".`
+        res.render('index', {book: bookInfo, error: null})
+      }else{
+        let bookInfo = `El libro \"${data[0].title}\" ha sido escrito por \"${data[0].author}\".`
+        res.render('index', {book: bookInfo, error: null})
+      }
+    })
+  .catch(error => console.log('error is', error));
 });
 
