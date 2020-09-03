c74aad831cc66a80325d1cc0b1d63f3e34c0d31a
FORGIS98 <cifradoforgis@gmail.com>
2020-09-03 19:58:43 +0200

    No se como enviar jsons a la api :/

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
   res.render('index', {book: null, error: null})
 });
 
+app.get('/addBook', function (req, res) {
+  res.render('addBook');
+});
+
 app.listen(3000, function () {
   console.log('The best web ever is running on port 3000!');
 });
@@ -23,7 +27,7 @@ app.post('/', function (req, res) {
   let isISBN = book.match(/^(97(8|9))?\d{9}(\d|X)$/)
 
   fetch(url, {
-    methos: "get"
+    method: "GET"
   })
     .then(response => {
       if (response.ok) {
@@ -42,6 +46,39 @@ app.post('/', function (req, res) {
         res.render('index', {book: bookInfo, error: null})
       }
     })
-  .catch(error => console.log('error is', error));
+    .catch(error => console.log('error is', error));
+});
+
+app.post('/addBook', function (req, res) {
+
+  let book = req.body.book;
+  let url = 'http://localhost:4000/api/books/add_book';
+
+  const dataToSend = JSON.stringify({
+      "isbn" : book[0],
+      "titulo" : book[1],
+      "autor" : book[2],
+      "edicion" : book[3],
+      "editorial" : book[4]
+    })
+
+
+  fetch(url, {
+    headers: { "Content-Type": "application/json" },
+    method: "post",
+    body: dataToSend
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        res.render('index', {book: null, error: 'Error: No se que ha ido mal :D'});
+      }
+    })
+    .then(data => {
+      res.render('index', {book: null, error: null})
+      console.log(data)
+    })
+    .catch(error => console.log('error is', error));
 });
 
